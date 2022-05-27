4746f684c3d74cbe0610159266708c5ef7d817a5
Nathan Westergard <m800mazdarx8@gmail.com>
2022-05-27 16:33:26 -0600

    Fixed password without tokens

--- password.js
+++ password.js
@@ -5,21 +5,9 @@ const reader = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
-  reader.question("Enter your password", function(input){
-	const tokens = input;
-	
-	const num0 = tokens[0];
-	const num1 = tokens[1];
-	const num2 = tokens[2];
-	const num3 = tokens[3];
-    const num4 = tokens[4];
-	const num5 = tokens[5];
-	const num6 = tokens[6];
-	const num7 = tokens[7];
-    const num8 = tokens[8];
-    const num9 = tokens[9];
-    const num10 = tokens[10];
 
+  reader.question("Enter your password \n", function(input){
+	
     if (input.length >= 10){
         console.log("success")
     } else {
